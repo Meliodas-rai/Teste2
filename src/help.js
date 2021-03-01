c1cf3f1761d64869f1af8ea557ffaa4ef05a65aa
Meliodas-rai <79414899+Meliodas-rai@users.noreply.github.com>
2021-03-01 14:58:46 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,195 @@
+const help = (prefix) => {
+	return `
+<══════════════════════>
+        *☢︎︎🇧 🇦 🇳  🇧 🇴 🇹☢︎︎ *
+ <══════════════════════>
+         
+ ㅤㅤ  ㅤ ㅤ █ㅤㅤㅤㅤㅤ   █
+         ㅤ █  ▄▀█ㅤ█▀▄ █
+ㅤㅤㅤㅤㅤ▐▌ㅤ ㅤ ㅤㅤㅤ ㅤ▐▌ㅤㅤㅤ𝙾 𝙱𝚁𝙰𝙱𝙾✔︎
+  ㅤ   ㅤㅤ█▌▀▄     ▄▄    ▄▀▐█
+  ㅤ  ㅤㅤ▐██     ▀▀    ▀▀     ██▌
+   ㅤ ㅤ ▄████▄   ▐▌ㅤ▄████▄
+
+          ✞︎Mᴇɴᴜ✞︎Bᴏᴛ✞︎       
+      *DONO*: Mᴇʟɪᴏᴅᴀs✔︎
+   Criador: Caussᵇʳᵃᵇᵒ   
+
+      ☠︎︎ ✞ ︎𝗖𝗼𝗺𝗮𝗻𝗱𝗼𝘀 𝗱𝗼 𝗯𝗼𝘁 ✞︎ ☠︎︎
+      
+  ☯︎ *informações*
+  ☠︎︎ Prefix: 「  ${prefix}  」
+  ☠︎︎ Canal do criador: https://youtube.com/channel/UCpB3qh2Sp3K23s9a2Q-Gf-g
+  
+  ☯︎ *SOBRE*
+  
+  ➪  ☠︎︎ ${prefix}info
+  ➪  ☠︎︎ ${prefix}blocklist
+  ➪  ☠︎︎ ${prefix}chatlist
+  ➪  ☠︎︎ ${prefix}ping
+  ➪  ☠︎︎ ${prefix}dono
+  ➪  ☠︎︎ ${prefix}appbot
+  ➪  ☠︎︎ ${prefix}bugreport
+  
+  ☯︎ *FAZER*
+  
+  ➪  ☠︎︎ ${prefix}sticker
+  ➪  ☠︎︎ ${prefix}stickergif
+  ➪  ☠︎︎ ${prefix}toimg
+  ➪  ☠︎︎ ${prefix}tomp3
+  ➪  ☠︎︎ ${prefix}bpink
+  ➪  ☠︎︎ ${prefix}marvellogo
+  ➪  ☠︎︎ ${prefix}snowwrite
+  ➪  ☠︎︎ ${prefix}3dtext
+  ➪  ☠︎︎ ${prefix}ninjalogo
+  ➪  ☠︎︎ ${prefix}water
+  ➪  ☠︎︎ ${prefix}firetext
+  ➪  ☠︎︎ ${prefix}logowolf
+  ➪  ☠︎︎ ${prefix}logowolf2
+  ➪  ☠︎︎ ${prefix}phlogo
+  ➪  ☠︎︎ ${prefix}glitch
+  ➪  ☠︎︎ ${prefix}neonlogo
+  ➪  ☠︎︎ ${prefix}neonlogo2
+  ➪  ☠︎︎ ${prefix}lionlogo
+  ➪  ☠︎︎ ${prefix}jokerlogo
+  ➪  ☠︎︎ ${prefix}shadow
+  ➪  ☠︎︎ ${prefix}burnpaper
+  ➪  ☠︎︎ ${prefix}coffee
+  ➪  ☠︎︎ ${prefix}lovepaper
+  ➪  ☠︎︎ ${prefix}woodblock
+  ➪  ☠︎︎ ${prefix}qowheart
+  ➪  ☠︎︎ ${prefix}mutgrass
+  ➪  ☠︎︎ ${prefix}undergocean
+  ➪  ☠︎︎ ${prefix}woodenboards
+  ➪  ☠︎︎ ${prefix}wolfmetal
+  ➪  ☠︎︎ ${prefix}metalictglow
+  ➪  ☠︎︎ ${prefix}8bit
+  ➪  ☠︎︎ ${prefix}ttp
+  ➪  ☠︎︎ ${prefix}herrypotter
+  ➪  ☠︎︎ ${prefix}pubglogo
+  ➪  ☠︎︎ ${prefix}quotemaker
+      
+  ☯︎ *MEDIA*
+
+  ➪  ☠︎︎ ${prefix}trendtwit
+  ➪  ☠︎︎ ${prefix}randomkpop
+  ➪  ☠︎︎ ${prefix}ytsearch
+  
+  ☯︎ *ESCOLA KKKKK*
+  
+  ➪  ☠︎︎ ${prefix}wiki
+  ➪  ☠︎︎ ${prefix}wikien
+  ➪  ☠︎︎ ${prefix}nulis
+  ➪  ☠︎︎ ${prefix}quotes
+  ➪  ☠︎︎ ${prefix}quotes2
+  ➪  ☠︎︎ ${prefix}artinama
+  
+  ☯︎ *KERANG AJAIB*
+  
+  ➪  ☠︎︎ ${prefix}apakah
+  ➪  ☠︎︎ ${prefix}kapankah
+  ➪  ☠︎︎ ${prefix}rate
+  ➪   ☠︎︎ ${prefix}bisakah
+  
+  ☯︎ *DOWNLOADER*
+  
+  ➪  ☠︎︎ ${prefix}images
+  ➪  ☠︎︎ ${prefix}ytmp3
+  ➪  ☠︎︎ ${prefix}ytmp4
+  ➪  ☠︎︎ ${prefix}tiktok
+  ➪  ☠︎︎ ${prefix}joox
+  
+  ☯︎ *MEME*
+  
+  ➪  ☠︎︎ ${prefix}meme
+  ➪  ☠︎︎ ${prefix}memeindo
+  
+  ☯︎ *SOM*
+  
+  ➪  ☠︎︎ ${prefix}play
+  ➪  ☠︎︎ ${prefix}tts
+  
+  ☯︎ *MÚSICA*
+  
+  ➪  ☠︎︎ ${prefix}lirik
+  ➪  ☠︎︎ ${prefix}chord
+  
+  ☯︎ *ISLAM*
+  
+  ➪  ☠︎︎ ${prefix}quran
+  
+  ☯︎ *STALK*
+  
+  ➪  ☠︎︎ ${prefix}tiktokstalk
+  ➪  ☠︎︎ ${prefix}igstalk
+      
+  ☯︎ *WIBU*
+  
+  ➪  ☠︎︎ ${prefix}neonime
+  ➪  ☠︎︎ ${prefix}pokemon
+  ➪  ☠︎︎ ${prefix}loli
+  ➪  ☠︎︎ ${prefix}waifu
+  ➪  ☠︎︎ ${prefix}randomanime
+  ➪  ☠︎︎ ${prefix}husbu
+  ➪  ☠︎︎ ${prefix}husbu2
+  ➪  ☠︎︎ ${prefix}wait
+  ➪  ☠︎︎ ${prefix}nekonime
+      
+  ☯︎ *DIVERSÃO*
+  
+  ➪  ☠︎︎ ${prefix}alay
+  ➪  ☠︎︎ ${prefix}gantengcek
+  ➪  ☠︎︎ ${prefix}watak
+  ➪  ☠︎︎ ${prefix}hobby
+  ➪  ☠︎︎ ${prefix}game
+  ➪  ☠︎︎ ${prefix}bucin
+  ➪  ☠︎︎ ${prefix}trust
+  ➪  ☠︎︎ ${prefix}dare
+  ➪  ☠︎︎ ${prefix}simi
+      
+  ☯︎ *INFORMAÇÃO*
+  ➪ ☠︎︎ ${prefix}bahasa
+  ➪  ☠︎︎ ${prefix}kodenegara
+  ➪  ☠︎︎ ${prefix}kbbi
+  ➪  ☠︎︎ ${prefix}fakta
+  ➪  ☠︎︎ ${prefix}infocuaca
+  ➪  ☠︎︎ ${prefix}infogempa
+  ➪  ☠︎︎ ${prefix}jadwaltvnow
+  ➪  ☠︎︎ ${prefix}covid
+      
+  ☯︎ *☠︎︎༒︎Cᴏᴍᴀɴᴅᴏs ᴅᴏ Mᴇʟɪᴏᴅᴀs༒︎☠︎︎*
+  
+  ➪  ☠︎︎ ${prefix}setprefix
+  ➪  ☠︎︎ ${prefix}block
+  ➪  ☠︎︎ ${prefix}bc
+  ➪  ☠︎︎ ${prefix}bcgc
+  ➪  ☠︎︎ ${prefix}clone
+  ➪  ︎︎︎☠︎︎ ${prefix}clearall
+  
+  ☯︎ *Não faço ideia da categoria desses*
+  
+  ➪  ☠︎︎ ${prefix}send
+  ➪  ☠︎︎ ${prefix}wame
+  ➪  ☠︎︎ ${prefix}virtex
+  ➪  ☠︎︎ ${prefix}exe
+  ➪  ☠︎︎ ${prefix}qrcode
+  ➪  ☠︎︎ ${prefix}afk
+  ➪  ☠︎︎ ${prefix}timer
+  ➪  ☠︎︎ ${prefix}fml
+  ➪  ☠︎︎ ${prefix}fml2
+  
+⠀        (\__/)
+          (•ㅅ•)  ㅤㅤ𝙿𝚎𝚒𝚝𝚊 𝚘 𝙱𝚘𝚝
+    ＿ノ ヽ ノ＼  __
+ /    `/ ⌒Ｙ⌒ Ｙ    ヽ
+(     (三ヽ人     /     |
+|    ﾉ⌒＼ ￣￣ヽ     ノ
+ヽ＿＿＿＞､＿＿_／
+         ｜( 王 ﾉ〈 
+         /ﾐ`ー―彡\  
+  
+  
+`
+}
+
+exports.help = help
